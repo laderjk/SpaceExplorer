4ab93befc10ddf34f9d17a04497ef727ce3676bf
Lader <megaladerjk@gmail.com>
2017-09-27 13:16:40 -0500

    initial commit

---
+++ Assets/Endless2DTerrain/Demos/Scripts/ChaseCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseCamera : MonoBehaviour {
+
+    public float speed = 20f;
+    GameObject hand;
+    // Update is called once per frame
+    void Update () {
+        hand = GameObject.Find("Car");
+        this.transform.position = new Vector3(hand.transform.position.x + (speed * Time.smoothDeltaTime), hand.transform.position.y, transform.position.z);
+	}
+}
